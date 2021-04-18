b5903b0721e241d5b9d27b7bd24ecc35604bb7cc
RitwickRaj78 <ritwickraj78@gmail.com>
2021-04-18 19:28:31 +0530

    [Models] Video Model Added

--- api/models.py
+++ api/models.py
@@ -2,3 +2,11 @@ from django.db import models
 
 
 # Create your models here.
+class YoutubeVideo(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.CharField(max_length=256)
+    published_date = models.DateTimeField()
+    thumbnail_url = models.URLField()
+
+    def __str__(self):
+        return self.title
